ddd080673665b406a111246627ec525e1ec7e4cd
PottaAkhil <96290098+PottaAkhil@users.noreply.github.com>
2025-09-18 11:58:20 +0530

    Update pemkey.tf

--- ec2/pemkey.tf
+++ ec2/pemkey.tf
@@ -1,4 +1,3 @@
-##############################################################################
 resource "tls_private_key" "rsa-4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -13,3 +12,4 @@ resource "local_file" "ssh_key" {
   filename = "${path.module}/keys/${aws_key_pair.kyc_app_public_key.key_name}.pem"
   content  = tls_private_key.rsa-4096.private_key_pem
 }
+
